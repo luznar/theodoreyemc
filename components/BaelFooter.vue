bbcadd5f206a459fd9fde9eff8c726c4f1880c15
luznar <77471110+luznar@users.noreply.github.com>
2023-05-02 21:58:48 -0400

    Update BaelFooter.vue

--- components/BaelFooter.vue
+++ components/BaelFooter.vue
@@ -4,59 +4,6 @@
       <div v-if="pagination.active" class="c-12 xs-text-left xs-p2">
         <lazy-bael-pagination :pagination="pagination" />
       </div>
-      <div
-        class="xs-text-left xs-p2 xs-border xs-border-right-none xs-border-bottom-none"
-        :class="signupAboutSize"
-      >
-        <div class="item">
-          <div v-if="siteDescription" class="footer__heading xs-mb2">About</div>
-          <p v-if="siteDescription">{{siteDescription}}</p>
-        </div>
-      </div>
-      <div
-        v-if="signupBoolean"
-        class="c-25 xs-text-left xs-p2 xs-border xs-border-right-none xs-border-bottom-none"
-      >
-        <lazy-send-grid />
-      </div>
-      <div
-        class="xs-text-left xs-p2 xs-border xs-border-right-none xs-border-bottom-none"
-        :class="signupAboutSize"
-      >
-        <div class="item">
-          <div v-if="connectData" class="footer__heading xs-mb2">Connect</div>
-          <ul v-if="connectData" class="list-unstyled">
-            <li v-for="(c,i) in connectData" :key="i">
-              <a :href="c.url">{{c.name}}</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div
-        class="xs-text-left xs-p2 xs-border xs-border-right-none xs-border-bottom-none"
-        :class="signupAboutSize"
-      >
-        <div class="item">
-          <div class="footer__heading xs-mb2">Deploy</div>
-
-          <a
-            href="https://app.netlify.com/start/deploy?repository=https://github.com/jake-101/bael-template"
-          >
-            <img
-              style="height:29px;width:auto;"
-              src="~/assets/deploy.svg"
-              title="Deploy to Netlify"
-            />
-          </a>
-        </div>
-      </div>
-      <div class="c-12 xs-text-left xs-p2 xs-border xs-border-right-none xs-border-bottom-none">
-        <div class="item xs-text-6">
-          <a href="https://github.com/jake-101/bael-template">Bael</a>
-          <span class="small-txt" v-if="version">{{version}}</span>, An open source design by
-          <a href="https://jake101.com">jake101</a>
-        </div>
-      </div>
     </div>
   </footer>
 </template>
